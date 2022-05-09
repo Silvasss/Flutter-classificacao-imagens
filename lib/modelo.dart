9c218674253f290883eab8080e460525a0b7940c
Silva <felipe_silva55@hotmail.com>
2022-05-09 07:38:44 -0700

    Versão final

--- lib/modelo.dart
+++ lib/modelo.dart
@@ -6,16 +6,14 @@ import 'package:image/image.dart' as image1;
 import 'package:image_picker/image_picker.dart';
 import 'package:teste/client.dart';
 
-
 class modelo extends StatefulWidget {
-  const modelo({Key? key}) : super(key : key);
+  const modelo({Key? key}) : super(key: key);
 
   @override
   _modeloState createState() => _modeloState();
 }
 
 class _modeloState extends State<modelo> {
-
   late File _image;
   late String _results;
   bool imageSelect = false;
@@ -27,36 +25,58 @@ class _modeloState extends State<modelo> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Image Classification"),
+        appBar: AppBar(
+          title: Text("Image Classification"),
+          centerTitle: true,
+        ),
+        /*
+      body: (imageSelect)?Center(
+        child: Image.asset(_image.path),
       ),
-      body: ListView(
-        children: [
-          (imageSelect)?Container(
-            margin: const EdgeInsets.all(10),
-            child: Image.file(_image),
-          ):Container(
-            margin: const EdgeInsets.all(10),
-            child: const Opacity(
-              opacity: 0.8,
-              child: Center(
-                child: Text("No image selected"),
+      */
+
+        body: ListView(
+          children: [
+            (imageSelect)
+                ? Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Image.file(_image, fit: BoxFit.fitWidth),
+                  )
+                : Container(
+                    margin: const EdgeInsets.all(10),
+                    child: const Opacity(
+                      opacity: 0.8,
+                      child: Center(
+                        child: Text("Sem imagem"),
+                      ),
+                    ),
+                  ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  (imageSelect)
+                      ? Text(
+                          _results,
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 20),
+                        )
+                      : const Text(
+                          "Sem resultado",
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        )
+                ],
               ),
-            ),
-          ),
-        ]
-      ),
-
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: pickImage,
-        tooltip: "Pick Image",
-        child: const Icon(Icons.image),
-      ),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: pickImage,
+          tooltip: "Pick Image",
+          child: const Icon(Icons.image),
+        ),
     );
   }
 
@@ -67,22 +87,14 @@ class _modeloState extends State<modelo> {
 
     File image = File(pickedFile!.path);
 
-    // Image no display do aplicativo
-    _image = image;
-
     // decodeImage will identify the format of the image and use the appropriate decode
     final image2 = image1.decodeImage(image.readAsBytesSync())!;
 
     // Image redimensionada e recortada quadrada
     final newImage = image1.copyResizeCropSquare(image2, 28);
 
-    final newImage2 = image1.grayscale(newImage);
-
     // Abre o arquivo, grava a lista de bytes nele e fecha o arquivo
-    image.writeAsBytesSync(image1.encodeJpg(newImage2));
-
-    // Upload de um texto sem image
-    //print(await clientService.uploadText());
+    image.writeAsBytesSync(image1.encodeJpg(newImage));
 
     // Upload de uma imagem com texto
     await clientService.uploadImage(image);
@@ -93,7 +105,12 @@ class _modeloState extends State<modelo> {
     // Chama o servidor django
     lista.add(await clientService.getAllDados());
 
-    _results = lista[0][0]['boby'];
+    setState(() {
+      imageSelect = true;
+      // Image no display do aplicativo
+      _image = image;
+      // Resultado do modelo
+      _results = lista[0][0]['boby'];
+    });
   }
-
 }
